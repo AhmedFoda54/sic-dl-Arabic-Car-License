03c2052fd9a9aef30e7577f1a9e8b3d843ff15e1
Ahmed <125858367+AhmedFoda54@users.noreply.github.com>
2024-10-29 00:24:47 +0300

    Update ocr_app.py

--- ocr_app.py
+++ ocr_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from PIL import Image
 from utils import crop_LowerPart_Plate, detect_text_easyocr, detect_text_yolo, process_video_with_plate_detection
 
-
 yolo_model = 'models/yolo11m_car_plate_trained.pt'
 ocr_yolo_model = 'models/yolo11m_car_plate_ocr.pt'
 
@@ -29,18 +28,15 @@ if uploaded_file is not None:
         st.image(cropped_image, caption="Cropped Car Plate", use_column_width=True)
 
         # Perform OCR on the cropped image
-        st.spinner("Performing OCR on the cropped image...")
-        easyocr_image, easyocr_text = detect_text_easyocr(cropped_image)
-        yolo_results, nums, chars = detect_text_yolo(ocr_yolo_model, cropped_image)
+        with st.spinner("Performing OCR on the cropped image..."):
+            easyocr_image, easyocr_text = detect_text_easyocr(cropped_image)
+            yolo_results, nums, chars = detect_text_yolo(ocr_yolo_model, cropped_image)
 
         st.subheader("YOLO OCR")
         st.image(yolo_results, caption="YOLO OCR", use_column_width=True)
         st.write(f"Detected Numbers: {nums} and detected Chars: {chars}")
 
         st.subheader("Easy OCR")
-        st.spinner("Performing Easy OCR on the image...")
-        # Display OCR results
-
         st.image(easyocr_image, caption="EasyOCR", use_column_width=True)
         if easyocr_text:
             st.write("Detected Text:")
@@ -62,8 +58,11 @@ if uploaded_video is not None:
     st.video(uploaded_video)
 
     with st.spinner("Processing video..."):
-        # Process the video and get the path to the saved output file
+        # Save processed video to /tmp directory and display it
         processed_video_path = process_video_with_plate_detection(uploaded_video, yolo_model, ocr_yolo_model)
-
-        # Display the processed video
-        st.video(processed_video_path)
+        
+        if processed_video_path:
+            # Display the processed video
+            st.video(processed_video_path)
+        else:
+            st.write("There was an issue processing the video.")
